4564f913f60988820c03bf15c5229ccd05aed020
Kristopher Eaton <kristophereaton12@gmail.com>
2022-06-07 10:45:36 -0700

    Blank app build complete

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+// Pass variable to configure component, selector = name etc...
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+
+})
+export class ServerComponent {}
